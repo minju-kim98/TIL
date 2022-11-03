87738390c8057d30b465673aa79f7af8b0244bcd
minju-kim98 <ducco705@snu.ac.kr>
2022-11-03 17:37:50 +0900

    Step 2

--- python_project/project1/main.py
+++ python_project/project1/main.py
@@ -1,6 +1,15 @@
 import requests
 import json
 
+def save_image(image_url, file_name):
+    """
+    이미지가 있는 image_url을 통해 file_name 파일로 저장하는 함수.
+    """
+    img_response = requests.get(image_url)
+    if img_response.status_code == 200:
+        with open(file_name, "wb") as fp:
+            fp.write(img_response.content)
+
 # 이미지 검색
 url = "https://dapi.kakao.com/v2/search/image"
 headers = {
