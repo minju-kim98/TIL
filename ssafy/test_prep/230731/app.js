0d86fdc86dce0ace9cf2361e3b215e123ae602ea
minju-kim98 <ducco705@snu.ac.kr>
2023-09-15 01:22:42 +0900

    test prep

---
+++ ssafy/test_prep/230731/app.js
@@ -0,0 +1,44 @@
+// var txt = prompt("Are you sure");
+// console.log(txt);
+// alert(typeof txt);
+
+
+// let a = document.children[0].children[1].children[1].textContent;
+// console.log(a);
+
+
+// let a = document.children[0].children[1].children[1];
+// a.textContent = "BBQ";
+
+
+// console.dir(a);
+// a.innerHTML = "KFC";
+
+
+// let a = document.querySelector("body > h1").textContent;
+// let b = document.querySelector("body > a");
+// b.innerHTML = "BBQ WORLD"
+// console.log(a);
+
+
+// const btn = document.querySelector("#btn");
+// btn.addEventListener('click', gogo);
+// function gogo(){
+//     alert("GOGO");
+// }
+
+
+// let btn2 = document.createElement('button');
+// let txt = document.createTextNode("Hi hello");
+// btn2.setAttribute("id", "BTS");
+// btn2.append(txt);
+// document.querySelector("body").append(btn2);
+
+
+// const div = document.querySelector("div");
+// div.classList.add("test");
+// div.classList.remove("test");
+// div.classList.toggle("test");
+// let a = div.classList.contains("test");
+// console.log(a);
+// div.classList.replace("test", "KFC");
