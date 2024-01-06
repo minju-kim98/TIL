c8c89ffd191b04293c6c0495d2a2eaa6647625d6
minju-kim98 <ducco705@snu.ac.kr>
2024-01-07 00:16:37 +0900

    tic-tac-toe

--- FE/react-tictactoe/src/App.js
+++ FE/react-tictactoe/src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Board from "./components/Board";
 
-function App() {
+const App = () => {
+  const [history, setHistory] = useState([
+    { squares: Array(9).fill(null), player: "O", winner: null },
+  ]);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const current = history[stepNumber];
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const handleClick = (i) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
+    const tmpWin = newCurrent.winner;
+
+    if (newSquares[i] === null) {
+      if (stepNumber % 2) {
+        newCurrent.player = "O";
+      } else {
+        newCurrent.player = "X";
+      }
+      newSquares[i] = newCurrent.player;
+      newCurrent.winner = calculateWinner(newSquares);
+      if (tmpWin !== null) return;
+
+      setHistory([
+        ...newHistory,
+        {
+          squares: newSquares,
+          player: newCurrent.player,
+          winner: newCurrent.winner,
+        },
+      ]);
+      setStepNumber(newHistory.length);
+    }
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button className="move-button" onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <h1>Tic Tac Toe</h1>
+      <div className="game-board">
+        <Board squares={current} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{moves}</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ FE/react-tictactoe/src/components/Board.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Square from "./Square";
+import "./Board.css";
+
+const Board = ({ squares, onClick }) => {
+  const renderSquare = (i) => {
+    return <Square value={squares.squares[i]} onClick={() => onClick(i)} />;
+  };
+
+  let player = squares.player;
+  if (player === "X") player = "O";
+  else player = "X";
+
+  const status = `Next player: ${player}`;
+  const win = `Winner is: ${squares.winner}`;
+
+  return (
+    <div>
+      <div className="board-wrapper">
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      {squares.winner === null && <div className="status">{status}</div>}
+      {squares.winner !== null && <div className="winner">{win}</div>}
+    </div>
+  );
+};
+
+export default Board;
